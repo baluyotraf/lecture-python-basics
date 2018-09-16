1e04e1b3df55c5210141d5f82ab8397f7fa4dc56
baluyotraf <baluyotraf@outlook.com>
2018-09-16 19:18:46 +0800

    Added sample codes for variables

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Variables Sample Code
+
+# Create and set variable var1 to 12
+var1 = 12
+
+# Create and set variable var3 to 1.0
+var2 = 1.0
+
+# Create and set variable var3 to hello world
+# Strings can use single quotes or double
+var3 = 'hello world'
+
+# Print the type of all variables
+print(type(var1), type(var2), type(var3))
