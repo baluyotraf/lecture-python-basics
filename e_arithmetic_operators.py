010bee06587c85590cc2cdb31811a344ddd767ee
baluyotraf <baluyotraf@outlook.com>
2018-09-16 19:55:07 +0800

    Added sample for arithmetic operators

---
+++ e_arithmetic_operators.py
@@ -0,0 +1,15 @@
+# Arithmetic Operations Sample Code
+
+x = 20
+y = 11
+
+print(x + y)  # Addition
+print(x - y)  # Subtraction
+print(x * y)  # Multiplication
+print(x / y)  # Division
+print(x % y)  # Modulus
+print(x ** y)  # Exponentiation
+print(x // y)  # Floor Division
+
+# Integer-Float Arithmetic
+print(x * 1.0)
