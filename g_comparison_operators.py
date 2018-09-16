25f493423a46420a3d9ee93d36b1504c50122185
baluyotraf <baluyotraf@outlook.com>
2018-09-16 20:28:32 +0800

    Added sample code for comparison

---
+++ g_comparison_operators.py
@@ -0,0 +1,13 @@
+# Comparison Operators Sample Code
+
+x = 20
+y = 11
+z = 11
+
+print(x == y)  # Equality
+print(x != y)  # Non-equality
+print(x > y)  # Greater Than
+print(x < y)  # Less Than
+print(x >= y)  # Greater Than or Equal
+print(y <= z)  # Less Than or Equal
+print(x > y >= z)  # Chained Comparison
