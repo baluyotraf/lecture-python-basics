f1f719de33c613389305f2f5382798d230e0f832
baluyotraf <baluyotraf@outlook.com>
2018-09-16 19:31:56 +0800

    Added source code for casting

---
+++ c_casting.py
@@ -0,0 +1,24 @@
+# Sample Casting Code
+
+# Define variables to cast
+var_int = 12
+var_float = 1.5
+var_string = '42'
+
+# Cast int to different types
+print('Int to different types')
+print(int(var_int), type(int(var_int)))
+print(float(var_int), type(float(var_int)))
+print(str(var_int), type(str(var_int)))
+
+# Cast float to different types
+print('Float to different types')
+print(int(var_float), type(int(var_float)))
+print(float(var_float), type(float(var_float)))
+print(str(var_float), type(str(var_float)))
+
+# Cast string to different types
+print('String to different types')
+print(int(var_string), type(int(var_string)))
+print(float(var_string), type(float(var_string)))
+print(str(var_string), type(str(var_string)))
