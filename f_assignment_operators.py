06ee4e5bdc7a19ae40d40662fcb51057e3ede237
baluyotraf <baluyotraf@outlook.com>
2018-09-16 20:05:14 +0800

    Added samples for assignment operators

---
+++ f_assignment_operators.py
@@ -0,0 +1,26 @@
+# Assignment Operations Sample Code
+
+x = 20  # Assignment
+print(x)
+
+x += 3  # x = x + 3
+print(x)
+
+x -= 3  # x = x - 3
+print(x)
+
+x *= 3  # x = x * 3
+print(x)
+
+x /= 3  # x = x / 3
+print(x)
+
+x %= 7  # x = x % 7
+print(x)
+
+x **= 2  # x = x ** 2
+print(x)
+
+x //= 7  # x = x // 7
+print(x)
+
