26c017a21a3ebe8eff25ef3092295f5826e47a29
baluyotraf <baluyotraf@outlook.com>
2018-09-16 20:29:12 +0800

    Added sample code for membership operator

---
+++ j_membership_operator.py
@@ -0,0 +1,16 @@
+# Membership Operator Sample Code
+
+string_1 = 'Hello World!'
+tuple_1 = tuple('abc')
+
+# Membership
+print('Hello' in string_1)
+print('Hi' in string_1)
+print('a' in tuple_1)
+print('d' in tuple_1)
+
+# Non-membership
+print('Hello' not in string_1)
+print('Hi' not in string_1)
+print('a' not in tuple_1)
+print('d' not in tuple_1)
