311db3415475fb7d0616fb1f6d104967d5da2d53
baluyotraf <baluyotraf@outlook.com>
2018-09-16 19:42:24 +0800

    Added file for string examples

---
+++ d_string.py
@@ -0,0 +1,19 @@
+# String Code Sample
+
+# String Definition
+string1 = 'Hello World!'
+string2 = "Hello World!"
+string3 = 'Hello ' "World!"
+
+print(string1, string2, string3)
+
+# String Concatenation
+print(string1 + string2)
+
+# String Multiplication
+print(string1 * 3)
+
+# String transformation
+print(string1.upper())
+print(string1.lower())
+print(string1.split(' '))
