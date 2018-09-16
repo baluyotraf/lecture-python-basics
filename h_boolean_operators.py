3b152b481c9963ac1ca2b71fa10c3e83281d88a4
baluyotraf <baluyotraf@outlook.com>
2018-09-16 20:28:43 +0800

    Added sample code for boolean ops

---
+++ h_boolean_operators.py
@@ -0,0 +1,13 @@
+# Boolean Operators Sample Code
+
+# and
+print(True and True)
+print(False and True)
+
+# or
+print(True or False)
+print(False or False)
+
+# not
+print(not True)
+print(not False)
