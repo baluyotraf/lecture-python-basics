a4e8dae9c6710fc92eb1cd51727e6b4dc0567c53
baluyotraf <baluyotraf@outlook.com>
2018-09-16 19:18:34 +0800

    Added samples for syntax

---
+++ syntax.py
@@ -0,0 +1,14 @@
+# Syntax Sample Code
+
+
+# We will wrap the print function
+# just to show how docstrings work
+def documented_print(message):
+    """
+    Prints the given message
+    :param message: message to print
+    """
+    print(message)
+
+
+documented_print('Hello World!')
