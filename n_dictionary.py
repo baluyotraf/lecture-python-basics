8992f73fc8b1ca5cdb0a9c45ffcdd4682de82ed0
baluyotraf <baluyotraf@outlook.com>
2018-09-18 22:02:52 +0800

    Added sample code for dictionary

---
+++ n_dictionary.py
@@ -0,0 +1,22 @@
+# Dictionary Sample Code
+
+
+# Dictionary Definition
+dict_1 = {
+    'string': 'hello',
+    1.0: 3.14,
+    10: 20,
+}
+
+
+# Access Members
+print(dict_1['string'])
+print(dict_1[10])
+
+# Add Members
+dict_1['add'] = 'new_value'
+print(dict_1)
+
+# Dictionary Length
+print(len(dict_1))
+
