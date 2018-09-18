40edbd0b50aa84bb2de00ca14fd98b8f8dd62b1c
baluyotraf <baluyotraf@outlook.com>
2018-09-18 22:02:25 +0800

    Added sample code for tuples

---
+++ l_tuple.py
@@ -0,0 +1,25 @@
+# Tuple Sample Code
+
+
+# Tuple Definition
+tuple_1 = ['hello', 1.2, 10]
+tuple_2 = [
+    'world',
+    3.14,
+    20,
+]
+
+
+# Access items
+# Read Only
+print(tuple_1[0])
+print(tuple_1[-1])
+print(tuple_1[0:2])
+
+# Unpacking
+a, b, c = tuple_1
+print(a, b, c)
+
+# Tuple Length
+print(len(tuple_1))
+
