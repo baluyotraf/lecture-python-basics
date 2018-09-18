ed868a57bdd2d8e3c50e3dac1bf81cc9c9c4bd9c
baluyotraf <baluyotraf@outlook.com>
2018-09-18 22:02:05 +0800

    Added source code for list

---
+++ k_list.py
@@ -0,0 +1,25 @@
+# List Sample Code
+
+
+# List Definition
+list_1 = ['hello', 1.2, 10]
+list_2 = [
+    'world',
+    3.14,
+    20,
+]
+
+
+# Access items
+# Read and Write
+print(list_1[0])
+print(list_1[-1])
+print(list_1[0:2])
+
+# Unpacking
+a, b, c = list_1
+print(a, b, c)
+
+# List Length
+print(len(list_1))
+
