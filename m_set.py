9359eff83730d572b9b2961106a5805297287cf7
baluyotraf <baluyotraf@outlook.com>
2018-09-18 22:02:39 +0800

    Added source code for set

---
+++ m_set.py
@@ -0,0 +1,22 @@
+# Set Sample Code
+
+
+# Set Definition
+set_1 = {'hello', 1.2, 10, 10, 'hello'}
+set_2 = {
+    'world',
+    3.14,
+    20,
+}
+
+# No duplicated allowed
+print(set_1)
+
+
+# Test Membership
+print('hello' in set_1)
+print(100 in set_1)
+
+# Set Length
+print(len(set_1))
+
