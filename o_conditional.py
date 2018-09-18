1a8dac75cb065e0d3154d2f1ab1721bb0458c91d
baluyotraf <baluyotraf@outlook.com>
2018-09-18 22:07:21 +0800

    Added sample code for conditional statement

---
+++ o_conditional.py
@@ -0,0 +1,22 @@
+# Conditional Sample Code
+
+if True:
+    print('On If')
+elif False:
+    print('On elif')
+else:
+    print('On else')
+
+if False:
+    print('On If')
+elif True:
+    print('On elif')
+else:
+    print('On else')
+
+if False:
+    print('On If')
+elif False:
+    print('On elif')
+else:
+    print('On else')
