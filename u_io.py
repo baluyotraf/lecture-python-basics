8ecfa4c30105b663ea77987ff7c16c16319efb9a
baluyotraf <baluyotraf@outlook.com>
2018-09-19 19:23:35 +0800

    Added source code for io

---
+++ u_io.py
@@ -0,0 +1,14 @@
+# Sample Code for Python IO
+
+
+x = input('Enter an integer: ')
+x = int(x)
+print(x)
+
+y = input('Enter a float: ')
+y = float(y)
+print(y)
+
+z = input('Enter a string: ')
+z = str(z)
+print(z)
