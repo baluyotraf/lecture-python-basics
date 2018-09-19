c6d72a24d02f0885797b821c0397be4d61d08d93
baluyotraf <baluyotraf@outlook.com>
2018-09-19 20:45:37 +0800

    Improved package and modules example

--- w_modules.py
+++ w_modules.py
@@ -1,15 +1,17 @@
 # Sample Code for accessing modules and package
 
 # Difference Import Types
-import w_module
 from w_package import module
-from w_module import func
+from w_package import func as init_func
 from w_package.module import func as package_func
-
+import w_module
+from w_module import func
 
 # Consume the functions on imported modules
-w_module.func()
 module.func()
-func()
+init_func()
 package_func()
+w_module.func()
+func()
+
 
